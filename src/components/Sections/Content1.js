a1767d2777f0756443764d02dfb42e6a5b8779c5
mostlyclicks <carlos@mostlyclicks.com>
2021-08-25 16:12:14 -0600

    reset

--- src/components/Sections/Content1.js
+++ src/components/Sections/Content1.js
@@ -7,7 +7,7 @@ const Content1 = () => {
       <div><h1>2022 Scott Addict</h1></div>
       <div><img src='../../../2022-Addict-RC-40.png' alt="bike" /></div>
       <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, voluptas saepe omnis quam doloremque sit nam quisquam optio voluptatum unde quo error quidem modi quasi! Vero recusandae quibusdam illum totam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Est soluta vitae officia. Libero suscipit repudiandae adipisci doloribus similique perferendis dolores aperiam impedit quaerat ullam enim possimus, quas vero doloremque ex?</p></div>
-      <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, voluptas saepe omnis quam doloremque sit nam quisquam optio voluptatum unde quo error quidem modi quasi! Vero recusandae quibusdam illum totam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Est soluta vitae officia. Libero suscipit repudiandae adipisci doloribus similique perferendis dolores aperiam impedit quaerat ullam enim possimus, quas vero doloremque ex?</p></div>
+      
     </Div>
   )
 }
@@ -17,8 +17,9 @@ export default Content1
 const Div = styled.div`
 
   display:flex;
-  flex-wrap:wrap;
+  
   gap:1rem;
+  align-content:center;
   
 
  div {
@@ -26,40 +27,19 @@ const Div = styled.div`
     outline:1px solid red;
     
     
+    
   }
 
   img {
-    width:400px;
-    height:400px;
-  }
-
-  div > p {
-    min-width:20ch;
-  }
-
-  
-
-  // @media (min-width:768px) {
-  //   outline:1px solid red;
-  //   display:flex;
+    width:100%;
     
-  //   h1 {
-  //     order:2;
-  //     flex-grow:1;
-      
-  //   }
-  //   img {
-  //     order:1;
-  //     width:400px;
-  //     height:400px;
-  //   }
-  //   p {
-  //     order:3;
-      
+    outline:1px solid green;
+    
+    
+  }
 
-  //   }
 
-  // }
+
 
 `
 
